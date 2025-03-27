6afdec594afadee7b64964ca5911feb1ea7e0528
João Paulo Salles de Oliveira <61626213+jpsallesdeoliveira@users.noreply.github.com>
2025-03-26 22:16:30 -0300

    processamento menu

--- chaveMestra.ino
+++ chaveMestra.ino
@@ -1,3 +1,5 @@
+String event;
+
 // Leitura da porta serial
 void SerialTask(void *pvParameters) {
     (void) pvParameters;
@@ -8,9 +10,114 @@ void SerialTask(void *pvParameters) {
             // Exibe a mensagem recebida no monitor serial
             Serial.print("Recebido: ");
             Serial.println(option);
+            // Processar opção recebida
+            processMenu(option);
         }
         // Pausa a task por 100ms para evitar sobrecarga e permitir o agendamento de outras tasks
         vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
+// Mostrar menu
+void showMenu() {
+    Serial.println("Menu:");
+    Serial.println("a. Cadastro do usuário");
+    Serial.println("b. Listagem dos nomes dos usuários cadastrados");
+    Serial.println("c. Listagem dos eventos");
+    Serial.println("d. Liberação da porta 1");
+    Serial.println("e. Liberação da porta 2");
+    event = "menu";
+}
+
+// Adicionar usuário
+void menuUser() {
+    Serial.println();
+    Serial.println("Cadastro do usuário:");
+    Serial.println("i. Nome");
+    Serial.println("ii. Senha");
+    Serial.println("iii. Definição do tipo de usuário  (Administrador ou não).");
+    event = "user";
+}
+
+// Menu nome
+void menuNome() {
+    Serial.println();
+    Serial.println("Digite o nome: ");
+    event = "nome";
+}
+
+// Adicionar nome
+void addNome(String nome) {
+    event = "add_nome";
+}
+
+// Menu senha
+void menuSenha() {
+    Serial.println();
+    Serial.println("Digite a senha: ");
+    event = "senha";
+}
+
+// Menu tipo
+void menuTipo() {
+    Serial.println();
+    Serial.println("Digite 1 para administrador ou 0 para não administrador: ");
+    event = "tipo";
+}
+
+// Listar nomes
+void obterNomes() {
+    Serial.println();
+    Serial.println("Lista dos nomes...");
+    Serial.println("i. Voltar para o menu principal");
+    event = "lista_nomes";
+}
+
+// Adicionar fila
+void addFila(String nome) {
+
+}
+
+// Processar opção
+void processMenu(String option) {
+    if (event == "menu") {
+        if (option == "a") {
+            menuUser();
+        } else if (option == "b") {
+            obterNomes();
+        } else if (option == "c") {
+
+        } else if (option == "d") {
+
+        } else if (option == "e") {
+
+        }
+    } else if (event == "user") {
+        if (option == "i") {
+            menuNome();
+        } else if (option == "ii") {
+            menuSenha();
+        } else if (option == "iii") {
+            menuTipo();
+        }
+    } else if (event == "events") {
+
+    } else if (event == "port1") {
+
+    } else if (event == "port2") {
+
+    } else if (event == "nome") {
+        addFila(option);
+        menuUser();
+    } else if (event == "senha") {
+
+    } else if (event == "tipo") {
+        
+    } else if (event == "lista_nomes") {
+        if (option == "i") {
+            Serial.println();
+            showMenu();
+        }
     }
 }
 
@@ -18,6 +125,10 @@ void SerialTask(void *pvParameters) {
 void setup() {
     // Inicializa a comunicação serial
     Serial.begin(115200);
+    delay(100);
+
+    // Mostrar menu
+    showMenu();
 
     // Cria a task para leitura serial
     xTaskCreate(
